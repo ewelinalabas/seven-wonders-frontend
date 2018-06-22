e632515cb2b9b69a93a99c481404dd647eb6fb3e
Damian Łabas <damian.labas@codibly.com>
2018-06-22 17:05:32 +0200

    add navigation menu

--- src/view/AppView/AppView.tsx
+++ src/view/AppView/AppView.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { SFC } from 'react';
-import { AppWrapper, ContentWrapper, MainWrapper } from 'view/AppView/AppView.s';
+import { AppWrapper, ContentWrapper, LeftNavigatonMenuWrapper, MainWrapper } from 'view/AppView/AppView.s';
 import { Route, Switch } from 'react-router';
 import { AppRoute } from 'app/route';
 import { NotFoundPage } from 'view/NotFoundPage/NotFoundPage';
 import { HomePageLoadable } from 'view/HomePage/HomePage.l';
 import { CounterPageLoadable } from 'view/CounterPage/CounterPage.l';
 import { TopBar } from 'view/AppView/TopNav/TopBar';
+import { NavigationMenu } from 'view/AppView/NavigationMenu/NavigationMenu';
 
 export const AppView: SFC = () => (
   <AppWrapper>
     <TopBar />
     <MainWrapper>
+      <LeftNavigatonMenuWrapper>
+        <NavigationMenu />
+      </LeftNavigatonMenuWrapper>
       <ContentWrapper>
         <Switch>
           <Route path={AppRoute.HOME} component={HomePageLoadable} exact />
---
+++ src/view/AppView/NavigationMenu/NavigationMenu.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { SFC } from 'react';
+import { NavigationMenuPaper } from 'view/AppView/NavigationMenu/NavigationMenu.s';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+
+export namespace NavigationMenu {
+  export type Props = {};
+}
+
+export const NavigationMenu: SFC<NavigationMenu.Props> = props => {
+  return (
+    <NavigationMenuPaper>
+      <List>
+        <ListItem primaryText="Some value"/>
+      </List>
+    </NavigationMenuPaper>
+  );
+};
